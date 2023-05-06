5c6b1dd906c2f04a7ca0e670eeba4010636b7848
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-05-06 19:37:31 +0800

    add: docker

--- backend/semantic_analysis/src/server.py
+++ backend/semantic_analysis/src/server.py
@@ -68,4 +68,4 @@ async def get(request):
     return json({"Compile coded by LinZhi": ""}, status=200)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8888)
+    app.run(host="0.0.0.0", port=8888)
